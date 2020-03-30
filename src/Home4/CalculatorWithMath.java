e66f9ec6a9dea0495a428050f92cffa6cb8cf4ad
NetBrainDel <62425209+NetBrainDel@users.noreply.github.com>
2020-03-30 16:40:51 +0300

    Add files via upload

---
+++ src/Home4/CalculatorWithMath.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.math.MathContext;
+import java.lang.Math;
+import java.lang.Object;
+public class CalculatorWithMath {
+
+        double a = 0;
+        double b = 0;
+
+        // деления, умножение, вычитание, сложение\\
+        double div = a / b;
+        double mul = a * b;
+        double sub = a - b;
+        double sum = a + b;
+
+        // Возведение в степень, Модуль числа, Корень из числа\\
+        double step = Math.pow(a,b);
+        double abs = Math.abs(a);
+        double kor = Math.sqrt(a);
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Home4/ICalculator.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface ICalculator {
+    double sum (double a,double b);
+    double div (double a,double b);
+    double mul (double a,double b);
+    double step (double a,double b);
+    double sub (double a,double b);
+    double abs(double a);
+    double kor(double a);
+}
