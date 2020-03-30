a1ac80dd4112ef2a2ee288ad9c08d7c4d946fd4b
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-03-30 12:27:17 +0300

    .

--- src/Home4/CountryAreaComparator.java
+++ src/Home4/CountryAreaComparator.java
@@ -4,14 +4,14 @@ import java.util.Comparator;
 
 public class CountryAreaComparator implements Comparator<ECountry> {
 
-    public int compare(ECountry country1, ECountry country2) {
-        if (country1.getAreaofthecountry() == country2.getAreaofthecountry()) {
+    public int compare(ECountry namecountry1, ECountry namecountry2) {
+        if (namecountry1.getAreaofthecountry() == namecountry2.getAreaofthecountry()) {
             return 0;
         }
-        if (country1.getAreaofthecountry() > country2.getAreaofthecountry()) {
+        if (namecountry1.getAreaofthecountry() > namecountry2.getAreaofthecountry()) {
             return 1;
         }
-        // country1.getAreaofthecountry() < country2.getAreaofthecountry()
+        // namecountry1.getAreaofthecountry() < namecountry2.getAreaofthecountry()
             return -1;
     }
 }
--- src/Home4/mainCountry.java
+++ src/Home4/mainCountry.java
@@ -6,22 +6,6 @@ import static Home4.ECountry.Russia;
 
 public class mainCountry {
     public static void main(String[] args) {
-        System.out.println("введите название города из списка:\n " +
-                "Russia\n" +
-                " Ukraine\n" +
-                " Poland\n" +
-                " Lithuania\n" +
-                " Latvia\n" +
-                " Belarus\n" +
-                " Austria\n" +
-                " Monaco\n" +
-                " USA\n" +
-                " CANADA");
-        Scanner in =  new Scanner( System.in);
-        System.out.println("введите название города №1: ");
-        String namecountry1 =  in.nextLine();
-        System.out.println("введите название города №2: ");
-        String namecountry2 =  in.nextLine();
 
         ICountry russia = ECountry.valueOfIgnoreCase(namecountry1);
         if (russia ==  ECountry.Russia) {
